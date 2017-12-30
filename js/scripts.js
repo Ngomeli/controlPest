da5d4822e68a6c23acc74f66145c26eef713b1e2
joshua ngomeli <joshuangomeli@gmail.com>
2017-12-30 09:54:20 +0300

    add information on mosquitoes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-
+var content= document.getElementById("content");
+var button= document.getElementById("show-more");
+button.onclick =function(){
+  if (content.className == "open") {
+    //shrink the box
+    content.className="";
+    button.innerHTML="SHOW MORE";
+  }
+  else {
+    //expand the box
+    content.className="open";
+    button.innerHTML="SHOW LESS";
+  }
+};
 $(document).ready(function(){
   $("button").click(function(){
     var box= document.getElementById("email");
